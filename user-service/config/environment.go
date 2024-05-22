8b78e2334344d419e0518ed43c42fc421cb57d37
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2024-05-22 12:36:58 +0700

    update env user service

--- user-service/config/environment.go
+++ user-service/config/environment.go
@@ -7,13 +7,17 @@ import (
 )
 
 type Environment struct {
-	AppPort string
+	AppPort        string
+	SecretPassword string
+	MysqlURI       string
 }
 
 func InitEnvironment() Environment {
 	godotenv.Load()
 
 	return Environment{
-		AppPort: os.Getenv("APP_PORT"),
+		AppPort:        os.Getenv("APP_PORT"),
+		SecretPassword: os.Getenv("SECRET_PASSWORD"),
+		MysqlURI:       os.Getenv("MYSQL_URI"),
 	}
 }
