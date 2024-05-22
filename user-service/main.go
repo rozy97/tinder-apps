384ba8ca1c334ea4e34ba6a09b025a7c79f0f310
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2024-05-22 12:55:03 +0700

    refactor main function

--- user-service/main.go
+++ user-service/main.go
@@ -2,46 +2,37 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 	"github.com/rozy97/tinder-apps/user-service/config"
+	"github.com/rozy97/tinder-apps/user-service/handler"
+	"github.com/rozy97/tinder-apps/user-service/repository"
+	"github.com/rozy97/tinder-apps/user-service/usecase"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	env := config.InitEnvironment()
+	db, err := sqlx.Connect("mysql", env.MysqlURI)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer db.Close()
+
+	repository := repository.NewRepository(db)
+	usecase := usecase.NewUsecase(repository, env)
+	handler := handler.NewHandler(usecase)
 
 	r := gin.Default()
 
-	r.POST("/register", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "register success",
-		})
-	})
+	r.POST("/register", handler.Register)
+	r.POST("/login", handler.Login)
+	r.POST("activation", handler.Activation)
 
-	r.POST("/login", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"access_token": gin.H{
-				"aud": "http://api.example.com",
-				"iss": "https://krakend.io",
-				"sub": "1234567890qwertyuio",
-				"jti": "mnb23vcsrt756yuiomnbvcx98ertyuiop",
-				"roles": []string{"role_a", "role_b"},
-				"exp": time.Now().Add(2 * time.Hour),
-			},
-			"refresh_token": gin.H{
-				"aud": "http://api.example.com",
-				"iss": "https://krakend.io",
-				"sub": "1234567890qwertyuio",
-				"jti": "mnb23vcsrt756yuiomn12876bvcx98ertyuiop",
-				"exp": time.Now().Add(7 * 24 * time.Hour),
-			},
-			"exp": time.Now().Add(2 * time.Hour),
-		})
-	})
-
-	err := r.Run(":" + env.AppPort) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err = r.Run(":" + env.AppPort) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
