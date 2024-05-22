160703e83e29a4c384848cda1c94831d0fc97718
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2024-05-22 14:11:57 +0700

    update header key is verified

--- library/user.go
+++ library/user.go
@@ -3,7 +3,7 @@ package library
 type UserHeader struct {
 	UserID     uint64     `header:"x-user-id" binding:"required"`
 	Status     UserStatus `header:"x-user-status" binding:"required"`
-	IsVerified uint8      `header:"is_verified" binding:"required"`
+	IsVerified uint8      `header:"x-user-is_verified" binding:"required"`
 	Gender     Gender     `header:"x-user-gender" binding:"required"`
 	CityID     uint64     `header:"x-user-city-id" binding:"required"`
 }
