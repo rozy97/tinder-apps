8290d83c25277463a0f5b2e52e431685525ea66a
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2024-05-22 12:46:31 +0700

    initiate usecase

--- user-service/usecase/usecase.go
+++ user-service/usecase/usecase.go
@@ -1,1 +1,27 @@
 package usecase
+
+import (
+	"context"
+
+	"github.com/rozy97/tinder-apps/user-service/config"
+	"github.com/rozy97/tinder-apps/user-service/model"
+)
+
+type Usecase struct {
+	repository Repository
+	env        config.Environment
+}
+
+func NewUsecase(repository Repository, env config.Environment) *Usecase {
+	return &Usecase{
+		repository: repository,
+		env:        env,
+	}
+}
+
+type Repository interface {
+	CountUserByEmail(ctx context.Context, email string) (total uint, err error)
+	InsertUser(ctx context.Context, user model.User) error
+	GetUserByEmail(ctx context.Context, email string) (user model.User, err error)
+	UpdateUserByID(ctx context.Context, user model.User) error
+}
